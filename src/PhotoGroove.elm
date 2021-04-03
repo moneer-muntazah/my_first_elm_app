120a4b59ba4f303b766bfcb2a11c54def3106430
Moneer Alhashim <mr.moneer@outlook.com>
2021-04-03 15:57:26 +0300

    added model

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -3,15 +3,40 @@ module PhotoGroove exposing (main)
 import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 
+
+urlPrefix =
+    ""
+
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            [ img [ src "https://elm-in-action/1.jpeg" ] [] 
-            , img [ src "https://elm-in-action/2.jpeg" ] []
-            , img [ src "https://elm-in-action/3.jpeg" ] []
-            ]
+            (List.map
+                (viewThumbnail model.selected)
+                model.photos
+            )
         ]
 
-main = 
-    view "no model yet"
+
+viewThumbnail selected thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , width 150
+        , height 150
+        , classList [ ( "selected", selected == thumb.url ) ]
+        ]
+        []
+
+
+initialModel =
+    { photos =
+        [ { url = "1.jpg" }
+        , { url = "2.jpg" }
+        , { url = "3.jpg" }
+        ]
+    , selected = "1.jpg"
+    }
+
+
+main =
+    view initialModel
