27b565331d1471623c4a035c218afe41242ab697
Moneer <mtzmoneer2020@gmail.com>
2021-04-17 21:06:01 +0300

    review

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,42 +1,42 @@
 module PhotoGroove exposing (main)
 
-import Browser
-import Html exposing (div, h1, img, text)
+import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
 
 
+type alias Photo =
+    { url : String }
+
+
+type alias Model =
+    { photos : List Photo, selected : String }
+
+
+urlPrefix : String
 urlPrefix =
     ""
 
 
+view : Model -> Html msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            (List.map
-                (viewThumbnail model.selected)
-                model.photos
-            )
-        , img
-            [ class "large"
-            , src (urlPrefix ++ model.selected)
-            ]
-            []
+        , div [ id "thumbnails" ] (List.map (viewThumbnail model.selected) model.photos)
         ]
 
 
+viewThumbnail : String -> Photo -> Html msg
 viewThumbnail selected thumb =
     img
         [ src (urlPrefix ++ thumb.url)
         , width 150
         , height 150
         , classList [ ( "selected", selected == thumb.url ) ]
-        , onClick { description = "ClickedPhoto", data = thumb.url }
         ]
         []
 
 
+initialModel : Model
 initialModel =
     { photos =
         [ { url = "1.jpg" }
@@ -46,18 +46,12 @@ initialModel =
     , selected = "1.jpg"
     }
 
-
-update msg model =
-    if msg.description == "ClickedPhoto" then
+update msg model = 
+    if msg.description == "ClickedPhoto" then 
         { model | selected = msg.data }
-
-    else
+    else 
         model
 
-
+main : Html msg
 main =
-    Browser.sandbox
-        { init = initialModel
-        , view = view
-        , update = update
-        }
+    view initialModel
